45c0ec6ca4ebf497a9e5896511817c344d7957a2
FeltSense <feltsense62@gmail.com>
2025-11-30 01:31:41 -0600

    🔧 Build Fix: Updated components/Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -100,5 +100,5 @@ export default function Hero() {
       {/* Bottom fade */}
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-slate-950 to-transparent" />
     </section>
-  </div></div></div></div></div></span>);
+  );
 }
